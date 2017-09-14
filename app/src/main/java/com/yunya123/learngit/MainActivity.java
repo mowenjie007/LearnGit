0444303a9c34777027960bc6172dae061f512cc6
mwj <mowenjie007@163.com>
2017-09-14 16:53:03 +0800

    添加了MainActivity

---
+++ app/src/main/java/com/yunya123/learngit/MainActivity.java
@@ -0,0 +1,10 @@
+package com.yunya123.learngit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yunya123 on 2017/9/14.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
